9558f30cf1d1d8256894da21f96bb34e92974060
mdisumma <69967922+mdisumma@users.noreply.github.com>
2021-12-02 12:47:40 +0000

    min

--- public/script.js
+++ public/script.js
@@ -3,8 +3,16 @@ const displayTable = document.querySelector("#displayTable")
 
     fetch("http://localhost:3000/api")
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+        console.log(data)
+        displayTable.innerHTML = `
+
+<ul class="leader">
+<li>${data[0].name}</li>
+<li>${data[0].country}</li>
+<li>${data[0].age}</li>
+</ul>
+`})
 
 
 		
-displayTable.innerHTML = "hello"
--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 import express from "express";
 import { createClient } from "@supabase/supabase-js";
 
-
 const app = express();
 const port = 3000;
 
+//SUPABASE CLIENT
 const SUPABASE_URL = "https://avvelquwyslzkodskshw.supabase.co";
 const SERVICE_KEY =
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjMzMzgzMTU1LCJleHAiOjE5NDg5NTkxNTV9.tmww3KW2ZpsLvhPlGq2es22MTmXuWK9Mp-wyGMvAtUY";
 
 const supabase = createClient(SUPABASE_URL, SERVICE_KEY);
-
-
-
 app.use(express.static("public"));
 
+//TABLE DATA
 app.get("/api", async (request, response) => {
 	let { data, error } = await supabase
 		.from("leaders")
@@ -23,6 +21,35 @@ app.get("/api", async (request, response) => {
 	response.send(data);
 	console.log(data)
 });
+//INSERT DATA
+app.post("/api", async (request, response) => {
+	let { data, error } = await supabase.from("leaders").insert([request.body]);
+	console.log(request.body);
+})
+
+//DELETE
+app.delete("/api", async (request, response) => {
+	let { data, error } = await supabase
+		.from("leaders")
+		.delete()
+		.match(request.body);
+	console.log(request.body);
+})
+
+//PUT
+app.put("/api", async (request, response) => {
+	console.log(request.body);
+
+	let { data, error } = await database
+		.from("leaders")
+		.update({
+			
+		})
+		.match({
+			
+		});
+});
+
 
 app.listen(port, () => {
 	console.log(`The server is listening on port ${port}.`);
